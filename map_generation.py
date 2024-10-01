35ec2b66d5709cb6f28a498e519c8dbfa7803ac8
Link165000 <149683873+Link165000@users.noreply.github.com>
2024-10-01 10:05:42 +0200

    Update map_generation.py

--- map_generation.py
+++ map_generation.py
@@ -1,22 +1,22 @@
+import random
+
 def generate_map(size):
     game_map = [['.' for _ in range(size)] for _ in range(size)]
-    
-    # Add biomes
+
     for y in range(size):
         for x in range(size):
-            if random.random() < 0.1:  # 10% chance to place a forest
+            if random.random() < 0.1:
                 game_map[y][x] = 'F'  # Forest
-            elif random.random() < 0.1:  # 10% chance to place ice
+            elif random.random() < 0.1:
                 game_map[y][x] = 'I'  # Ice
-            elif random.random() < 0.1:  # 10% chance to place water
+            elif random.random() < 0.1:
                 game_map[y][x] = 'W'  # Water
-            elif random.random() < 0.05:  # 5% chance for an enemy
+            elif random.random() < 0.05:
                 game_map[y][x] = 'E'  # Enemy
-    
+
     return game_map
 
-def get_terrain_type(x, y):
-    # Return the type of terrain at (x, y)
+def get_terrain_type(game_map, x, y):
     terrain_types = {
         '.': 'Plain',
         'F': 'Forest',
