69c02b33511e6bb0bd8acb3ac4c13a9f5d36f3ee
Byzaddo <egeakbulut113@gmail.com>
2024-10-08 19:52:43 +0200

    made a class system for weapons for easier use

--- game/Weapons.py
+++ game/Weapons.py
@@ -12,42 +12,80 @@
 #   health_regen = if the weapon heals you
 #   spread = how many monsters damaged at a time
 
-def starter_sword():
-    damage_delt = 1
-    exp_given = 0.5
-    health_regen = 0
-    spread = 1
 
-def starter_bow():
-    damage_delt = 1
-    range_of = 2
-    exp_given = 0.5
-    health_regen = 0
-    spread = 1
-   
-def intermediate_sword():
-    damage_delt = 3
-    exp_given = 2
-    health_regen = 0
-    spread = 1
+class Weapon():
+    def __init__(self, damage_delt, exp_given, health_regen, spread):
+        self.damage_dealt = damage_delt
+        self.exp_given = exp_given
+        self.health_regen = health_regen
+        self.spread = spread
+        
+    def attack(self):
+        return f"dealt {self.damage_dealt} damage. spread to {self.spread} enemies"
     
-def intermediate_bow():
-    damage_delt = 2
-    range_of = 5
-    exp_given = 1.5
-    health_regen = 0
-    spread = 1
+    
+class MeleeWeapon(Weapon):
+    def __init__(self, damage_delt, exp_given, health_regen, spread):
+        super().__init__(damage_delt, exp_given, health_regen, spread)
+        
+class RangedWeapon(Weapon):
+    def __init__(self, damage_delt, range_of, exp_given, health_regen, spread):
+        super().__init__(damage_delt, exp_given, health_regen, spread)
+        self.range_of = range_of
+        
+    def attack(self):
+        return f"Dealt {self.damage_delt} damage at range {self.range_of}. Spread to {self.spread} enemies."
+    
+    
+    
+starter_sword = MeleeWeapon(damage_delt=1, exp_given=0.5, health_regen=0, spread=1)
+starter_bow = RangedWeapon(damage_delt=1, range_of=2, exp_given=0.5, health_regen=0, spread=1)
 
-def scythe():
-    damage_delt = 20
-    exp_given = 10
-    health_regen = 10
-    spread = 3
+intermediate_sword = MeleeWeapon(damage_delt=3, exp_given=2, health_regen=0, spread=1)
+intermediate_bow = RangedWeapon(damage_delt=2, range_of=5, exp_given=1.5, health_regen=0, spread=1)
 
-def katana():
-    damage_delt = 10
-    exp_given = 30
-    health_regen = 15
-    spread = 5
+scythe = MeleeWeapon(damage_delt=20, exp_given=10, health_regen=10, spread=3)
+katana = MeleeWeapon(damage_delt=10, exp_given=30, health_regen=15, spread=5)
+        
+    
+        
+
+# def starter_sword():
+#     damage_delt = 1
+#     exp_given = 0.5
+#     health_regen = 0
+#     spread = 1
+
+# def starter_bow():
+#     damage_delt = 1
+#     range_of = 2
+#     exp_given = 0.5
+#     health_regen = 0
+#     spread = 1
+   
+# def intermediate_sword():
+#     damage_delt = 3
+#     exp_given = 2
+#     health_regen = 0
+#     spread = 1
+    
+# def intermediate_bow():
+#     damage_delt = 2
+#     range_of = 5
+#     exp_given = 1.5
+#     health_regen = 0
+#     spread = 1
+
+# def scythe():
+#     damage_delt = 20
+#     exp_given = 10
+#     health_regen = 10
+#     spread = 3
+
+# def katana():
+#     damage_delt = 10
+#     exp_given = 30
+#     health_regen = 15
+#     spread = 5
 
 
