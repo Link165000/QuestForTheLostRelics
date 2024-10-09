ed8bb3a4ffa4138f10f57a74c402c6a7fe851179
Byzaddo <egeakbulut113@gmail.com>
2024-10-09 14:12:32 +0200

    added try catch for better error and input handling

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ def login():
     print("Welcome to Quest for the Lost Relics!")
     
     action = input("Do you want to (1) Register or (2) Login? ")
-    
-    if action == '1':
-        username = input("Enter a new username: ")
-        password = input("Enter a password: ")
-        register_user(username, password)
-    elif action == '2':
-        username = input("Enter your username: ")
-        password = input("Enter your password: ")
-        if login_user(username, password):
-            print(f"Welcome back, {username}!")
-        else:
-            print("Login failed.")
-    else:
-        print("Invalid option. Exiting.")
+    try:
+        if action == '1':
+           username = input("Enter a new username: ")
+           password = input("Enter a password: ")
+           register_user(username, password)
+        elif action == '2':
+            username = input("Enter your username: ")
+            password = input("Enter your password: ")
+            if login_user(username, password):
+              print(f"Welcome back, {username}!")
+            else:
+               print("Login failed.")
+    except ValueError:
+        print("must be a number between 1 or 2") 
 
 
 def main():
