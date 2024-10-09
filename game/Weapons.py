533694c29dd2d4c48a3cd0fb26ba2ef7adee8655
Byzaddo <egeakbulut113@gmail.com>
2024-10-09 09:55:36 +0200

    someone fucked up the whole code

--- game/Weapons.py
+++ game/Weapons.py
@@ -45,11 +45,6 @@ intermediate_sword = MeleeWeapon(damage_delt=3, mastery_given=2, health_regen=0,
 intermediate_bow = RangedWeapon(damage_delt=2, range_of=5, mastery_given=1.5, health_regen=0, spread=1)
 
 
-scythe = MeleeWeapon(damage_delt=20, exp_given=10, health_regen=10, spread=3)
-katana = MeleeWeapon(damage_delt=10, exp_given=30, health_regen=15, spread=5)
-
-
-
 scythe = MeleeWeapon(damage_delt=20, mastery_given=10, health_regen=10, spread=3)
 katana = MeleeWeapon(damage_delt=10, mastery_given=2, health_regen=15, spread=5)
         
